acd9727e18a11cf1b552d38dacd8b95e74d9fc27
Adriana Villamor <adriana.villamor@ices.dk>
2023-10-04 11:27:35 +0200

    First check of sid and sag mismatches

--- bootstrap/SAG_data.R
+++ bootstrap/SAG_data.R
@@ -1,18 +1,31 @@
 library(icesTAF)
 library(icesFO)
 
-# out <- load_sag(2022, "Faroes")
-# 
-# sag_complete <- out
-# write.taf(out, file = "SAG_complete_FO.csv", quote = TRUE)
-# 
-# 
-# status <- load_sag_status(2022)
-# write.taf(status, file = "bootstrap/data/SAG_data/SAG_status.csv", quote = TRUE)
-# 
+
+#Previous check of last year content in SAG and in SID
+
+sid <- load_sid(2023)
+sag23 <- icesSAG::getListStocks(2023)
+sag23 <- sag23 %>% filter(Purpose == "Advice")
 
 
-#before 2022 we downloaded the whole SAG. Might make sense to get back this function
+#1. Check those that should have been assessed in 2023 and were not, according to SID:
+check <- sid%>% filter(YearOfNextAssessment == 2023)
+#they appear in sag_complete, but they should be checked anyway as it is wrong in sid
 
+#2. Check mismatches between sag22 and sid with assessment in 2022
+sid23 <- sid%>% filter(YearOfLastAssessment == 2023)
+
+#what is in sag and not in sid
+check <- setdiff(sag23$StockKeyLabel,sid23$StockKeyLabel)
+
+#what is in sid and not in sag
+#these do not show up in sag_complete, none of the assessments
+check <- setdiff(sid23$StockKeyLabel, sag23$StockKeyLabel)
+
+#Luca, do you think we need to check something else?
 
 sag_complete <- load_sag_complete(2022)
+
+
+
